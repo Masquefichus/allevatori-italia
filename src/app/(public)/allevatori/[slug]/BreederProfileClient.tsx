8f3f717ec60218440d6cb886bb3a31ae14568af0
Masquefichus <amadiscleva@icloud.com>
2026-04-05 15:38:53 +0900

    Remove scrollbar on tabs by switching overflow-x-auto to hidden

--- src/app/(public)/allevatori/[slug]/BreederProfileClient.tsx
+++ src/app/(public)/allevatori/[slug]/BreederProfileClient.tsx
@@ -568,7 +568,7 @@ export default function BreederProfileClient({
           </div>
 
           {/* Tabs */}
-          <div className="flex gap-0 -mx-4 sm:-mx-6 lg:-mx-8 px-4 sm:px-6 lg:px-8 overflow-x-auto">
+          <div className="flex gap-0 -mx-4 sm:-mx-6 lg:-mx-8 px-4 sm:px-6 lg:px-8 overflow-x-hidden">
             {([
               { id: "panoramica", label: "Panoramica" },
               { id: "cucciolate", label: "Cucciolate" },
